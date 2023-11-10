98ab368b6a8dc3de89965f284178ca8b36049f5f
YoseC <yo.cifuentes@duocuc.cl>
2023-11-10 12:29:38 -0300

    ajustando tutorial 3

--- src/app/pages/home/tuto03/tuto03.page.ts
+++ src/app/pages/home/tuto03/tuto03.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tuto03',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tuto03Page implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
+  }
+
+  irTutorial04() {
+    this.router.navigate(['/home/tuto04'])
+  }
+  irRegistro() {
+    this.router.navigate(['/registro'])
   }
 
 }
