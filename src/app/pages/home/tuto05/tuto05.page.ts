59febb3fadbe31ad0fd94c918ddb42588480fa51
YoseC <yo.cifuentes@duocuc.cl>
2023-11-14 15:22:42 -0300

    tutorial 5

--- src/app/pages/home/tuto05/tuto05.page.ts
+++ src/app/pages/home/tuto05/tuto05.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tuto05',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tuto05.page.scss'],
 })
 export class Tuto05Page implements OnInit {
-
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
+  }
+
+  irTutorial05() {
+    this.router.navigate(['/home'])
+  }
+  irRegistro() {
+    this.router.navigate(['/registro'])
   }
 
 }
